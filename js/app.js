6733486d361d4b60300060ea58c32397e24902a9
Jaime <jaimepovedafernandez@gmail.com>
2023-04-10 23:59:43 +0200

    autenticacion commit 2
    cambios y pruebas

--- js/app.js
+++ js/app.js
@@ -1,41 +1,81 @@
-//import { createClient } from "@supabase/supabase-js";
-const { createClient } = supabase;
-
-const supabaseUrl = "https://afribcvcanlurbnjrioe.supabase.co";
-const supabaseKey =
+const SUPABASE_URL = "https://afribcvcanlurbnjrioe.supabase.co";
+const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcmliY3ZjYW5sdXJibmpyaW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMjUzMTgsImV4cCI6MTk5NjcwMTMxOH0.YaVIKtzBY5bwnzvtvwebTUkM1GoSG86aNbgdgijGhbE";
 
-//const supabase = createClient(supabaseUrl, supabaseKey);
-const _supabase = createClient(supabaseUrl, supabaseKey);
-//console.log("Supabase Instance: ", _supabase);
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.userToken = null;
 
-$("#registerButton").click()(function () {
-  console.log("hola");
-})
+document.addEventListener("DOMContentLoaded", function (event) {
+  var signUpForm = document.querySelector("#sign-up");
+  signUpForm.onsubmit = signUpSubmitted.bind(signUpForm);
 
-//Registro
-/* console.log(
-  await _supabase.auth.signUp({
-    email: "example@email.com",
-    password: "example-password",
-  })
-); */
+  var logInForm = document.querySelector("#log-in");
+  logInForm.onsubmit = logInSubmitted.bind(logInForm);
 
+  var userDetailsButton = document.querySelector("#user-button");
+  userDetailsButton.onclick = fetchUserDetails.bind(userDetailsButton);
 
-//Login
-/* console.log("Login:");
-console.log(
-  await _supabase.auth.signInWithPassword({
-    email: "example@email.com",
-    password: "example-password",
-  })
-);
+  var logoutButton = document.querySelector("#logout-button");
+  logoutButton.onclick = logoutSubmitted.bind(logoutButton);
+});
 
-console.log("Session:");
-console.log(await _supabase.auth.getSession());
-console.log("User:");
-console.log(await _supabase.auth.getUser()); */
+const signUpSubmitted = (event) => {
+  event.preventDefault();
+  const email = event.target[0].value;
+  const password = event.target[1].value;
 
+  supabase.auth
+    .signUp({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
 
-/* const { data, error } = await _supabase.auth.getSession()
-console.log(data); */
+const logInSubmitted = (event) => {
+  event.preventDefault();
+  const email = event.target[0].value;
+  const password = event.target[1].value;
+
+  supabase.auth
+    .signIn({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response);
+    })
+    .catch((err) => {
+      alert(err.response.text);
+    });
+};
+
+const fetchUserDetails = () => {
+  alert(JSON.stringify(supabase.auth.user()));
+};
+
+const logoutSubmitted = (event) => {
+  event.preventDefault();
+
+  supabase.auth
+    .signOut()
+    .then((_response) => {
+      document.querySelector("#access-token").value = "";
+      document.querySelector("#refresh-token").value = "";
+      alert("Logout successful");
+    })
+    .catch((err) => {
+      alert(err.response.text);
+    });
+};
+
+function setToken(response) {
+  if (response.user.confirmation_sent_at && !response?.session?.access_token) {
+    alert("Confirmation Email Sent");
+  } else {
+    document.querySelector("#access-token").value =
+      response.session.access_token;
+    document.querySelector("#refresh-token").value =
+      response.session.refresh_token;
+    alert("Logged in as " + response.user.email);
+  }
+}
---
+++ js/appPruebas.js
@@ -0,0 +1,107 @@
+//import { createClient } from "@supabase/supabase-js";
+const { createClient } = supabase;
+
+const supabaseUrl = "https://afribcvcanlurbnjrioe.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcmliY3ZjYW5sdXJibmpyaW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMjUzMTgsImV4cCI6MTk5NjcwMTMxOH0.YaVIKtzBY5bwnzvtvwebTUkM1GoSG86aNbgdgijGhbE";
+
+//const supabase = createClient(supabaseUrl, supabaseKey);
+const _supabase = createClient(supabaseUrl, supabaseKey);
+//console.log("Supabase Instance: ", _supabase);
+
+if (localStorage.getItem("User") === "") {
+  $("#logZone").append(`
+    <a href="login.html" class="btn btn-primary">Login</a>
+  `)
+} else {
+  $("#logZone").append(`
+    Hola `+ localStorage.getItem("User") + `
+    <a href="index.html" id="logOutButton" class="btn btn-danger">Logout</a>
+  `)
+}
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var signUpForm = document.querySelector('#sign-up')
+  signUpForm.onsubmit = signUpSubmitted.bind(signUpForm)
+});
+
+const signUpSubmitted = (event) => {
+  event.preventDefault()
+  console.log("hola");
+  const email = event.target[0].value
+  const password = event.target[1].value
+
+  _supabase.auth
+    .signUp({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : console.log(response)
+    })
+    .catch((err) => {
+      alert(err)
+    })
+}
+
+/* $(document).ready(async function () {
+  $("#registerButton").click(async function () {
+
+    //signInWithEmail($("#registerEmail")[0].value, $("#registerPassword")[0].value);
+
+    console.log("hola");
+
+    let email = $("#registerEmail")[0].value;
+    let password = $("#registerPassword")[0].value;
+    _supabase.auth
+      .signUp({ email, password })
+      .then((response) => {
+        response.error ? alert(response.error.message) : console.log(response)
+      })
+      .catch((err) => {
+        alert(err)
+      })
+
+    localStorage.setItem("User", email);
+  })
+  $("#loginButton").click(function () {
+    localStorage.setItem("User", "Jaime");
+  })
+  $("#logOutButton").click(function () {
+    localStorage.setItem("User", "");
+  })
+});
+
+async function signInWithEmail(email, password) {
+  console.log("hola");
+  console.log(await _supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  }))
+  localStorage.setItem("User", email);
+} */
+
+//Registro
+/* console.log(
+  await _supabase.auth.signUp({
+    email: "example@email.com",
+    password: "example-password",
+  })
+); */
+
+
+//Login
+/* console.log("Login:");
+console.log(
+  await _supabase.auth.signInWithPassword({
+    email: "example@email.com",
+    password: "example-password",
+  })
+);
+
+console.log("Session:");
+console.log(await _supabase.auth.getSession());
+console.log("User:");
+console.log(await _supabase.auth.getUser()); */
+
+
+/* const { data, error } = await _supabase.auth.getSession()
+console.log(data); */
