3ec3ee705c65b3a53945d218f8c495dbe5d9f34a
Jaime <jaimepovedafernandez@gmail.com>
2023-04-11 11:28:44 +0200

    autenticacion commit 3
    funcional

--- js/app.js
+++ js/app.js
@@ -2,80 +2,81 @@ const SUPABASE_URL = "https://afribcvcanlurbnjrioe.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcmliY3ZjYW5sdXJibmpyaW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMjUzMTgsImV4cCI6MTk5NjcwMTMxOH0.YaVIKtzBY5bwnzvtvwebTUkM1GoSG86aNbgdgijGhbE";
 
-var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-window.userToken = null;
+const SUPABASE = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+if (localStorage.getItem("User") === "") {
+  $("#logZone").append(`
+    <a href="login.html" class="btn btn-primary">Login</a>
+  `);
+} else {
+  $("#logZone").append(
+    `
+    Hola ` +
+      localStorage.getItem("User") +
+      `
+    <button id="logOutButton" class="btn btn-danger">Logout</button>
+  `
+  );
+}
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  var signUpForm = document.querySelector("#sign-up");
-  signUpForm.onsubmit = signUpSubmitted.bind(signUpForm);
-
-  var logInForm = document.querySelector("#log-in");
-  logInForm.onsubmit = logInSubmitted.bind(logInForm);
-
-  var userDetailsButton = document.querySelector("#user-button");
-  userDetailsButton.onclick = fetchUserDetails.bind(userDetailsButton);
-
-  var logoutButton = document.querySelector("#logout-button");
-  logoutButton.onclick = logoutSubmitted.bind(logoutButton);
+  $("#signUpForm").submit(signUp);
+  $("#logInForm").submit(logIn);
+  $("#logOutButton").click(logOut);
 });
 
-const signUpSubmitted = (event) => {
+function signUp(event) {
   event.preventDefault();
-  const email = event.target[0].value;
-  const password = event.target[1].value;
+  let email = event.target[0].value;
+  let password = event.target[1].value;
 
-  supabase.auth
+  SUPABASE.auth
     .signUp({ email, password })
     .then((response) => {
-      response.error ? alert(response.error.message) : setToken(response);
+      if (response.error) {
+        alert(response.error.message);
+      } else {
+        localStorage.setItem("User", response.data.user.email);
+        window.location.href = "index.html";
+      }
     })
     .catch((err) => {
       alert(err);
     });
-};
+}
 
-const logInSubmitted = (event) => {
+function logIn(event) {
   event.preventDefault();
-  const email = event.target[0].value;
-  const password = event.target[1].value;
+  let email = event.target[0].value;
+  let password = event.target[1].value;
 
-  supabase.auth
-    .signIn({ email, password })
+  SUPABASE.auth
+    .signInWithPassword({ email, password })
     .then((response) => {
-      response.error ? alert(response.error.message) : setToken(response);
+      if (response.error) {
+        alert(response.error.message);
+      } else {
+        localStorage.setItem("User", response.data.user.email);
+        window.location.href = "index.html";
+      }
     })
     .catch((err) => {
-      alert(err.response.text);
+      alert(err);
     });
-};
+}
 
-const fetchUserDetails = () => {
-  alert(JSON.stringify(supabase.auth.user()));
-};
-
-const logoutSubmitted = (event) => {
+function logOut(event) {
   event.preventDefault();
 
-  supabase.auth
+  SUPABASE.auth
     .signOut()
     .then((_response) => {
-      document.querySelector("#access-token").value = "";
-      document.querySelector("#refresh-token").value = "";
+      console.log(_response);
+      localStorage.setItem("User", "");
       alert("Logout successful");
+      window.location.href = "index.html";
     })
     .catch((err) => {
       alert(err.response.text);
     });
-};
-
-function setToken(response) {
-  if (response.user.confirmation_sent_at && !response?.session?.access_token) {
-    alert("Confirmation Email Sent");
-  } else {
-    document.querySelector("#access-token").value =
-      response.session.access_token;
-    document.querySelector("#refresh-token").value =
-      response.session.refresh_token;
-    alert("Logged in as " + response.user.email);
-  }
 }
